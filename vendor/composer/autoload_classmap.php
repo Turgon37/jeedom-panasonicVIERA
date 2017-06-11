f402435cefd16200fa2b2db57d147f557d436f15
Pierre GINDRAUD <pgindraud@gmail.com>
2017-06-11 12:38:56 +0200

    Update composer autoloader

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,7 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Phpwol\\Factory' => $vendorDir . '/tomnomnom/phpwol/Phpwol/Factory.php',
-    'Phpwol\\MagicPacket' => $vendorDir . '/tomnomnom/phpwol/Phpwol/MagicPacket.php',
-    'Phpwol\\Socket' => $vendorDir . '/tomnomnom/phpwol/Phpwol/Socket.php',
 );
