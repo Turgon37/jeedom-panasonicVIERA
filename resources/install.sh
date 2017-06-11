36e7c29f815547a392b7046fae0c00852f3d07af
Pierre GINDRAUD <pgindraud@gmail.com>
2017-06-11 21:28:45 +0200

    Add chmod +X for 3rdparty

--- resources/install.sh
+++ resources/install.sh
@@ -1,15 +1,25 @@
 touch /tmp/dependancy_panasonicVIERA_in_progress
-echo 0 > /tmp/dependancy_panasonicVIERA_in_progress
 echo "Launch install of PanasonicViera dependancy"
+echo 0 > /tmp/dependancy_panasonicVIERA_in_progress
+echo 'Configure 3rdParty'
+chmod +x ../../plugins/panasonicVIERA/3rdparty/panasonic_viera_adapter.py
+
+echo 10 > /tmp/dependancy_panasonicVIERA_in_progress
+echo 'Download panasonic-viera library'
 cd /tmp
 sudo rm -R /tmp/panasonic-viera >/dev/null 2>&1
 git clone https://github.com/Turgon37/panasonic-viera.git
+
 echo 50 > /tmp/dependancy_panasonicVIERA_in_progress
+echo 'Install panasonic-viera library'
 cd /tmp/panasonic-viera
 echo 'y' | sudo pip --quiet uninstall panasonic_viera
 sudo python setup.py install
+
 echo 95 > /tmp/dependancy_panasonicVIERA_in_progress
+echo 'Cleaning...'
 sudo rm -R /tmp/panasonic-viera >/dev/null 2>&1
+
 echo 100 > /tmp/dependancy_panasonicVIERA_in_progress
 echo "Everything is successfully installed!"
 rm /tmp/dependancy_panasonicVIERA_in_progress
