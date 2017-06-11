77e022c5cc2b367b746764612496991356ae8991
Pierre GINDRAUD <pgindraud@gmail.com>
2017-06-11 12:32:37 +0200

    Update install script

--- resources/install.sh
+++ resources/install.sh
@@ -2,11 +2,14 @@ touch /tmp/dependancy_panasonicVIERA_in_progress
 echo 0 > /tmp/dependancy_panasonicVIERA_in_progress
 echo "Launch install of PanasonicViera dependancy"
 cd /tmp
-rm -R /tmp/panasonic-viera >/dev/null 2>&1
+sudo rm -R /tmp/panasonic-viera >/dev/null 2>&1
 git clone https://github.com/Turgon37/panasonic-viera.git
 echo 50 > /tmp/dependancy_panasonicVIERA_in_progress
 cd /tmp/panasonic-viera
+echo 'y' | sudo pip --quiet uninstall panasonic_viera
 sudo python setup.py install
+echo 95 > /tmp/dependancy_panasonicVIERA_in_progress
+sudo rm -R /tmp/panasonic-viera >/dev/null 2>&1
 echo 100 > /tmp/dependancy_panasonicVIERA_in_progress
 echo "Everything is successfully installed!"
 rm /tmp/dependancy_panasonicVIERA_in_progress
