afdf138c78b2ff637a4f592db797b8144b921c3a
Pierre GINDRAUD <pgindraud@gmail.com>
2017-10-07 01:00:01 +0200

    remove olg image path

--- desktop/php/panasonicVIERA.php
+++ desktop/php/panasonicVIERA.php
@@ -50,7 +50,7 @@ $eqLogics = eqLogic::byType('panasonicVIERA');
 <?php foreach ($eqLogics as $eqLogic) : $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive'); ?>
             <div class="eqLogicDisplayCard cursor" data-eqLogic_id="<?= $eqLogic->getId() ?>" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
                 <center>
-                    <img src="plugins/panasonicVIERA/doc/images/panasonicVIERA_icon.png" height="105" width="95" />
+                    <img src="plugins/panasonicVIERA/plugin_info/panasonicVIERA_icon.png" height="105" width="95" />
             	</center>
             	<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center><?= $eqLogic->getHumanName(true, true) ?></center></span>
             </div>
